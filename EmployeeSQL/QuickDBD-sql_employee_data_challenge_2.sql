42096927596c0b8901945df99e4f5e31ef840191
Lisa Tschudi <lizabethnt@gmail.com>
2024-02-17 10:58:39 -0800

    sql file saved with table creation, some queries working

--- EmployeeSQL/QuickDBD-sql_employee_data_challenge_2.sql
+++ EmployeeSQL/QuickDBD-sql_employee_data_challenge_2.sql
@@ -74,3 +74,39 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+--BEGIN QUERIES
+--List  the employee number, last name, first name, sex and salary for each employee:
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM employees AS e
+INNER JOIN salaries AS s ON
+e.emp_no = s.emp_no;
+
+--List the first name, last name, and hire date for the employees who were hired in 1986 
+SELECT first_name, last_name, hire_date
+FROM employees AS e
+Where EXTRACT(YEAR FROM e.hire_date) = 1986;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name 
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager as dm
+JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name 
+SELECT de.dept_no, de.emp_no, e.last_name, e.first_name, d.dept_name
+FROM dept_emp AS de
+JOIN departments AS d ON de.dept_no = d.dept_no
+JOIN employees AS e ON de.emp_no = e.emp_no;
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B
+SELECT first_name, last_name, sex
+FROM employees as e
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
+
+--List each employee in the Sales department, including their employee number, last name, and first name
+SELECT emp_no, last_name, first_name
+FROM employees
+
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name 
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) 
+
