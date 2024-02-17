5739ae4bba1613476292239adb7c9293f534b713
Lisa Tschudi <lizabethnt@gmail.com>
2024-02-17 10:57:22 -0800

    table creation, data importation and some queries working

--- EmployeeSQL/QuickDBD-sql_employee_data_challenge.sql
+++ EmployeeSQL/QuickDBD-sql_employee_data_challenge.sql
@@ -2,7 +2,7 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/UZvXdM
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-
+-- Set up all six tables and relationships between them:
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
     "emp_title" VARCHAR   NOT NULL,
@@ -74,3 +74,29 @@ REFERENCES "employees" ("emp_no");
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
 
+--BEGIN QUERIES
+--List  the employee number, last name, first name, sex and salary for each employee:
+SELECT e.emp_no, first_name, last_name, s.salary
+FROM employees AS e
+INNER JOIN salaries AS s ON
+e.emp_no = s.emp_no;
+
+SELECT * FROM employees
+
+--List the first name, last name, and hire date for the employees who were hired in 1986 
+SELECT first_name, last_name, hire_date
+FROM employees AS e
+Where EXTRACT(YEAR FROM e.hire_date) = 1986;
+
+--List the manager of each department along with their department number, department name, employee number, last name, and first name 
+SELECT dm.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+FROM dept_manager as dm
+JOIN departments AS d ON dm.dept_no = d.dept_no
+JOIN employees AS e ON dm.emp_no = e.emp_no;
+
+--List the department number for each employee along with that employee’s employee number, last name, first name, and department name 
+
+--List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B 
+--List each employee in the Sales department, including their employee number, last name, and first name 
+--List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name 
+--List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name) 
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sql-challenge
 assignment for UofO/edX Data Analysis Bootcamp using SQL
 Starter files and all data provided by UofO/edX
+
+Please note: Importation of provided CSV files into tables works in the following order:
+titles
+departments
+employees
+dept_employee
+dept_manager
+salaries
